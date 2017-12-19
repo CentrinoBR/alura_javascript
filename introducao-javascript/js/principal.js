1bdb4babb8fa6197335a60cec3005031c4250fe3
Railson Rames <rames.cloud@outlook.com.br>
2017-12-19 18:01:58 -0200

    Update na chamada para o cálculo

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -2,16 +2,33 @@ var titulo = document.querySelector(".titulo");
 titulo.textContent = "Novo título passado por JS";
 
 
-var paciente = document.querySelector(".paciente");
+var trPaciente = document.querySelector(".paciente");
 
-var tdPeso = paciente.querySelector(".info-peso");
+var tdPeso = trPaciente.querySelector(".info-peso");
 var peso = tdPeso.textContent;
 
-var tdAltura = paciente.querySelector(".info-altura");
+var tdAltura = trPaciente.querySelector(".info-altura");
 var altura = tdAltura.textContent;
 
-var tdImc = paciente.querySelector(".info-imc");
+var tdImc = trPaciente.querySelector(".info-imc");
 var imc = tdImc.textContent;
 
-imc = peso/(altura*altura);
-tdImc.textContent = imc;
+var pesoValido = true;
+var alturaValida = true;
+
+if ((peso <= 0) || (peso >= 1000)) {
+  console.log("Peso inválido!");
+  pesoValido = false;
+  tdImc.textContent = "Peso inválido!";
+}
+
+if ((altura <= 0) || (altura >= 3.00)) {
+  console.log("Altura inválida!");
+  alturaValida = false;
+  tdImc.textContent = "Altura inválida!";
+}
+
+if (pesoValido && alturaValida) {
+  imc = peso/(altura*altura);
+  tdImc.textContent = imc;
+}
