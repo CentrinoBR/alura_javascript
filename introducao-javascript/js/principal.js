cf70253ee8ecdbf35052a0cc868722b86001265a
Railson Rames <rames.cloud@outlook.com.br>
2017-12-22 16:02:03 -0200

    Alteração para captura dinâmica dos dados com querySelectorAll

--- introducao-javascript/js/principal.js
+++ introducao-javascript/js/principal.js
@@ -1,34 +1,39 @@
 var titulo = document.querySelector(".titulo");
 titulo.textContent = "Novo título passado por JS";
 
+var pacientes = document.querySelectorAll(".paciente");
+var tamanhoListaPacientes = pacientes.length;
 
-var trPaciente = document.querySelector(".paciente");
+for (var i = 0; i < tamanhoListaPacientes; i++) {
+  console.log(pacientes[i]); //so pra ver o array no console, com cada um dos elementos que o querySelectorAll conseguiu capturar
 
-var tdPeso = trPaciente.querySelector(".info-peso");
-var peso = tdPeso.textContent;
+  var tdPeso = pacientes[i].querySelector(".info-peso");
+  var peso = tdPeso.textContent;
 
-var tdAltura = trPaciente.querySelector(".info-altura");
-var altura = tdAltura.textContent;
+  var tdAltura = pacientes[i].querySelector(".info-altura");
+  var altura = tdAltura.textContent;
 
-var tdImc = trPaciente.querySelector(".info-imc");
-var imc = tdImc.textContent;
+  var tdImc = pacientes[i].querySelector(".info-imc");
+  var imc = tdImc.textContent;
 
-var pesoValido = true;
-var alturaValida = true;
+  var pesoValido = true;
+  var alturaValida = true;
 
-if ((peso <= 0) || (peso >= 1000)) {
-  console.log("Peso inválido!");
-  pesoValido = false;
-  tdImc.textContent = "Peso inválido!";
-}
+  if ((peso <= 0) || (peso >= 1000)) {
+    console.log("Peso inválido!");
+    pesoValido = false;
+    tdImc.textContent = "Peso inválido!";
+    pacientes[i].style.backgroundColor = "lightcoral";
+  }
 
-if ((altura <= 0) || (altura >= 3.00)) {
-  console.log("Altura inválida!");
-  alturaValida = false;
-  tdImc.textContent = "Altura inválida!";
-}
+  if ((altura <= 0) || (altura >= 3.00)) {
+    console.log("Altura inválida!");
+    alturaValida = false;
+    tdImc.textContent = "Altura inválida!";
+  }
 
-if (pesoValido && alturaValida) {
-  imc = peso/(altura*altura);
-  tdImc.textContent = imc;
+  if (pesoValido && alturaValida) {
+    imc = peso/(altura*altura);
+    tdImc.textContent = imc.toFixed(2);
+  }
 }
